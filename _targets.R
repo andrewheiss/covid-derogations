4a2d2fb130c0fd7ad9ed0e775560da832a146849
Andrew Heiss <andrew@andrewheiss.com>
2021-03-26 16:55:53 -0400

    Remove old example code

--- _targets.R
+++ _targets.R
@@ -81,23 +81,3 @@ list(
                csl = csl),
              format = "file")
 )
-#
-# list(
-#   tar_target(
-#     raw_data_file,
-#     "data/raw_data.csv",
-#     format = "file"
-#   ),
-#   tar_target(
-#     raw_data,
-#     read_csv(raw_data_file, col_types = cols())
-#   ),
-#   tar_target(
-#     data,
-#     raw_data %>%
-#       mutate(Ozone = replace_na(Ozone, mean(Ozone, na.rm = TRUE)))
-#   ),
-#   tar_target(hist, create_plot(data)),
-#   tar_target(fit, biglm(Ozone ~ Wind + Temp, data)),
-#   tar_render(report, "index.Rmd")
-# )
